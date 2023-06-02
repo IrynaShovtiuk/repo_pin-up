4fb33fa768bfe935276007c374bb232ebc93d84d
IrynaShovtiuk <shovtiuk.iryna@gmail.com>
2023-06-02 13:58:10 +0300

    Add commit

---
+++ tests/script1.spec.js
@@ -0,0 +1,17 @@
+const {test, expect} = require('@playwright/test')
+
+test('First test', async ({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/')
+    await page.waitForTimeout(2000)
+    await expect(page.locator ('[class="heading"]')).toHaveText('Welcome to the-internet')
+    await page.locator('[href="/login"]').click()
+    await page.waitForTimeout(2000)
+    await expect(page.locator ('h2')).toHaveText('Login Page')
+    await page.locator('[id="username"]').fill('tomsmith')
+    await page.waitForTimeout(2000)
+    await page.locator('[id="password"]').fill('SuperSecretPassword!')
+    await page.waitForTimeout(2000)
+    await page.locator('[class="fa fa-2x fa-sign-in"]').click()
+    await expect(page.locator('[id="flash"]')).toContainText('You logged into a secure area!')
+    await page.waitForTimeout(5000)
+})
