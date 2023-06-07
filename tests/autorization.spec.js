5e30176b8dd0e427fa3071b345d92eb8bfbd43a5
IrynaShovtiuk <shovtiuk.iryna@gmail.com>
2023-06-07 12:53:21 +0300

    rework add pause

--- tests/autorization.spec.js
+++ tests/autorization.spec.js
@@ -2,6 +2,7 @@ const {test, expect} = require('@playwright/test');
 
 test('login user by email and check security page', async ({page}) => {
     await page.goto('https://pin-up.ua/');
+    await page.waitForTimeout(3_000);
     await page.locator('[data-testid="loginBtn"]').click();
     await page.locator('[data-testid="loginFormAuthInput"]').fill('paw_patrol@zabarna.net'); 
     await page.locator('[data-testid="loginFormPasswordInput"]').fill('paw_patrol'); 
